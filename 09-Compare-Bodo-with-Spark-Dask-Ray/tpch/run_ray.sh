d279560e963f02ff85d4472fd125fc862704fe04
duihuhu <duihuhu@163.com>
2022-10-17 19:17:37 +0800

    add parameter

--- 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray.sh
+++ 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-for((i=1; i<=100; i++))
+for((i=1; i<=22; i++))
 do
-    python3.9 ray_queries_statical.py $i
-    sleep 10
-done
+    for((j=1; j<=100; j++))
+    do
+        python3.9 ray_queries_statical.py $i
+        sleep 10
+    done
+done 
