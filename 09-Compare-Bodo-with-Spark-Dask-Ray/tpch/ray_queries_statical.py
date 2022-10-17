02e631a047ad99565a2d27ab74aceaea1d07da97
duihuhu <duihuhu@163.com>
2022-10-17 18:33:45 +0800

    add remove output warning

--- 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/ray_queries_statical.py
+++ 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/ray_queries_statical.py
@@ -7,13 +7,12 @@ The differences are in:
 2. Query 8 : total.groupby("O_YEAR", as_index = False).apply(udf) --> total.groupby("O_YEAR").apply(udf).reset_index()
 
 """
-from warnings import simplefilter
-simplefilter(action='ignore', category=FutureWarning)
+import warnings
+warnings.filterwarnings('ignore')
 import ray
 import modin.pandas as pd
 import time
-# import warnings
-# warnings.filterwarnings('ignore', category=FutureWarning)
+
 
 #ray.init()
 ray.init(address = "auto")
