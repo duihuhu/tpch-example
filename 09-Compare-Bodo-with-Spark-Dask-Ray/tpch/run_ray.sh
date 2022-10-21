469c010cbc2c6999ec42123fceba2c9173264fab
duihuhu <duihuhu@163.com>
2022-10-21 18:28:43 +0800

    edit run ray

--- 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray.sh
+++ 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ulimit -n 65535
 k=1
-for((i=10; i<=22; i++))
+for((i=1; i<=22; i++))
 do
     for((j=1; j<=15; j++))
     do
         python3 -W ignore::DeprecationWarning ray_queries_statical.py $i 'single'
         sleep 5
         k=$(($k+1))
-        if [ $k -gt 5 ]
+        if [ $k -gt 2 ]
         then
             ray stop
             ray start --head --port=6379 --num-cpus 40 
