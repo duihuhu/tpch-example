e490f40404781b6b877f553d0f5bdd0e6957460e
duihuhu <duihuhu@163.com>
2022-10-22 10:11:35 +0800

    debug run_ray_cluster_ssh

--- 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray_cluster_ssh.sh
+++ 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray_cluster_ssh.sh
@@ -9,7 +9,6 @@ function login_start(){
     ulimit -n 65535
     cd /usr/local/python3.8.10/bin
     ./ray start  --address='10.118.0.221:6379' --num-cpus 40
-    sleep 5
     exit
 EOF
 }
@@ -19,7 +18,6 @@ function login_stop(){
     ssh $user@$ip > /dev/null 2>&1 << EOF
     cd /usr/local/python3.8.10/bin
     ./ray stop
-    sleep 3
     exit
 EOF
 }
@@ -27,29 +25,26 @@ EOF
 
 ulimit -n 65535
 ray start --head --port=6379 --num-cpus 40 
-sleep 2
 login_start
-sleep 2
-# k=1
-# for((i=1; i<=22; i++))
-# do
-#     for((j=1; j<=40; j++))
-#     do
-#         python3 -W ignore::DeprecationWarning ray_queries_statical.py $i 'cluster'
-#         sleep 2
-#         k=$(($k+1))
-#         if [ $k -gt 3 ]
-#         then
-#             login_stop
-#             sleep 2
-#             ray stop
-#             sleep 2
-#             ulimit -n 65535
-#             ray start --head --port=6379 --num-cpus 40 
-#             sleep 2
-#             login_start
-#             sleep 2
-#             k=0
-#         fi
-#     done
-# done 
+sleep 5
+k=1
+for((i=1; i<=22; i++))
+do
+    for((j=1; j<=40; j++))
+    do
+        python3 -W ignore::DeprecationWarning ray_queries_statical.py $i 'cluster'
+        sleep 2
+        k=$(($k+1))
+        if [ $k -gt 3 ]
+        then
+            login_stop
+            ray stop
+            sleep 5
+            ulimit -n 65535
+            ray start --head --port=6379 --num-cpus 40 
+            login_start
+            sleep 5
+            k=0
+        fi
+    done
+done 
