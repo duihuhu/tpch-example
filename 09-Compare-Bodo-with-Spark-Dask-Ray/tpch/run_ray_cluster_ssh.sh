028e2ad81382b4055927304a8d62e68b973326b1
duihuhu <duihuhu@163.com>
2022-10-22 09:52:14 +0800

    add run_ray_cluster_ssh

---
+++ 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray_cluster_ssh.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+user="root"
+ip="10.118.0.222"
+passwd="hucc@1qaz"
+
+function login_start(){
+    ssh $user@$ip > /dev/null 2>&1 << EOF
+    ulimit -n 65535
+    cd /usr/local/python3.8.10/bin
+    ./ray start  --address='10.118.0.221:6379' --num-cpus 40
+    sleep 3
+    exit
+EOF
+}
+
+
+function login_stop(){
+    ssh $user@$ip > /dev/null 2>&1 << EOF
+    ulimit -n 65535
+    cd /usr/local/python3.8.10/bin
+    ./ray stop
+    sleep 3
+    exit
+EOF
+}
+
+
+ulimit -n 65535
+ray start --head --port=6379 --num-cpus 40 
+sleep 2
+login_start
+sleep 2
+k=1
+for((i=1; i<=22; i++))
+do
+    for((j=1; j<=40; j++))
+    do
+        python3 -W ignore::DeprecationWarning ray_queries_statical.py $i 'cluster'
+        sleep 2
+        k=$(($k+1))
+        if [ $k -gt 3 ]
+        then
+            login_stop
+            sleep 2
+            ray stop
+            sleep 2
+            ray start --head --port=6379 --num-cpus 40 
+            sleep 2
+            login_start
+            sleep 2
+            k=0
+        fi
+    done
+done 
