be173f26e42d6709e3151320ca2f00b6009f55b0
duihuhu <duihuhu@163.com>
2022-11-04 11:34:29 +0800

    three node run

--- 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray_cluster_ssh_three_node.sh
+++ 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray_cluster_ssh_three_node.sh
@@ -48,7 +48,7 @@ for((i=1; i<=1; i++))
 do
     for((j=1; j<=40; j++))
     do
-        python3 -W ignore::DeprecationWarning ray_queries.py 2& > log$j.txt
+        python3 -W ignore::DeprecationWarning ray_queries.py 2& > ../data/log$j.txt
         sleep 2
         login_stop
         ray stop
