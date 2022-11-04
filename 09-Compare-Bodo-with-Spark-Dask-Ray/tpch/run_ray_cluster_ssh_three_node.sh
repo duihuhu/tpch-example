9f79ec6a8dd7da4255648f66080cf7d8dcde9056
duihuhu <duihuhu@163.com>
2022-11-04 14:07:43 +0800

    three node run

--- 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray_cluster_ssh_three_node.sh
+++ 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray_cluster_ssh_three_node.sh
@@ -8,6 +8,7 @@ ip2="10.118.0.224"
 function login_start(){
     ssh $user@$ip > /dev/null 2>&1 << EOF
     ulimit -n 65535
+    export RAY_BACKEND_LOG_LEVEL=error
     cd /usr/local/python3.8.10/bin
     ./ray start  --address='10.118.0.221:6379' --num-cpus 40 --disable-usage-stats
     exit
@@ -17,6 +18,7 @@ EOF
 function login_start1(){
     ssh $user@$ip2 > /dev/null 2>&1 << EOF
     ulimit -n 65535
+    export RAY_BACKEND_LOG_LEVEL=error
     ray start  --address='10.118.0.221:6379' --num-cpus 40 --disable-usage-stats
     exit
 EOF
@@ -41,6 +43,7 @@ EOF
 export RAY_BACKEND_LOG_LEVEL=error
 ulimit -n 65535
 ray start --head --port=6379 --num-cpus 40 --disable-usage-stats  --include-dashboard=false
+sleep 5
 login_start
 login_start1
 sleep 6
@@ -58,6 +61,7 @@ do
         export RAY_BACKEND_LOG_LEVEL=error
         ulimit -n 65535
         ray start --head --port=6379 --num-cpus 40 --disable-usage-stats  --include-dashboard=false
+        sleep 5
         login_start
         login_start1
         sleep 6
