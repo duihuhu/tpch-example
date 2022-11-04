b9a63b4ea226dbbafc041158228640156078e2a4
duihuhu <duihuhu@163.com>
2022-11-04 11:44:37 +0800

    three node run

--- 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray_cluster_ssh_three_node.sh
+++ 09-Compare-Bodo-with-Spark-Dask-Ray/tpch/run_ray_cluster_ssh_three_node.sh
@@ -7,21 +7,20 @@ ip2="10.118.0.224"
 
 function login_start(){
     ssh $user@$ip > /dev/null 2>&1 << EOF
-    echo "aa"
-    ulimit -n 65535
-    cd /usr/local/python3.8.10/bin
-    ./ray start  --address='10.118.0.221:6379' --num-cpus 40 --disable-usage-stats
-    exit
-EOF
-    sleep 2
-    ssh $user@$ip2 > /dev/null 2>&1 << EOF
-    echo "aa"
     ulimit -n 65535
     cd /usr/local/python3.8.10/bin
     ./ray start  --address='10.118.0.221:6379' --num-cpus 40 --disable-usage-stats
     exit
 EOF
 }
+
+function login_start1(){
+    ssh $user@$ip2 > /dev/null 2>&1 << EOF
+    ulimit -n 65535
+    cd /usr/local/python3.8.10/bin
+    ./ray start  --address='10.118.0.221:6379' --num-cpus 40 --disable-usage-stats
+    exit
+EOF
 
 
 function login_stop(){
@@ -30,7 +29,9 @@ function login_stop(){
     ./ray stop
     exit
 EOF
-    sleep 2
+}
+
+function login_stop1(){
     ssh $user@$ip2 > /dev/null 2>&1 << EOF
     cd /usr/local/python3.8.10/bin
     ./ray stop
@@ -38,11 +39,14 @@ EOF
 EOF
 }
 
+
 export RAY_BACKEND_LOG_LEVEL=error
 ulimit -n 65535
 ray start --head --port=6379 --num-cpus 40 --disable-usage-stats  --include-dashboard=false
 login_start
-sleep 3
+sleep 2
+login_start1
+sleep 2
 
 # for((i=1; i<=1; i++))
 # do
